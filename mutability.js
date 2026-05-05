fa08d47290a104ec9efcf044eff83a5991e585cf
Amonoff <ochukaamon@gmail.com>
2026-05-05 20:19:46 +0300

    learning mutability

---
+++ mutability.js
@@ -0,0 +1,18 @@
+const clone1 = { ...person }
+const clone2 = Object.assign({}, person)
+
+//reference to same object
+const samePerson = person
+
+//modify original
+person.age += 1
+person.country = 'FR'
+
+console.log(person.age === 78);      // true
+console.log(person.country === "FR"); // true
+console.log(clone1.age === 77);      // true
+console.log(clone1.country === "US"); // true
+console.log(clone2.age === 77);      // true
+console.log(clone2.country === "US"); // true
+console.log(samePerson.age === 78);  // true
+console.log(samePerson.country === "FR"); // true
