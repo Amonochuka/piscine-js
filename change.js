d5abb75caf5e61fd42e66815ee4eb91b65cceeee
Amonoff <ochukaamon@gmail.com>
2026-05-05 21:58:33 +0300

    making changes

--- change.js
+++ change.js
@@ -14,17 +14,17 @@ function set(key, value) {
     return value
 }
 
-// Test get
-console.log(get("num"))   // 42
-console.log(get("bool"))  // true
-console.log(get("str"))   // 'some text'
+// // Test get
+// console.log(get("num"))   // 42
+// console.log(get("bool"))  // true
+// console.log(get("str"))   // 'some text'
 
-// Test set
-console.log(set("num", 77))        // 77
-console.log(set("bool", false))    // false
-console.log(set("str", "new text")) // 'new text'
+// // Test set
+// console.log(set("num", 77))        // 77
+// console.log(set("bool", false))    // false
+// console.log(set("str", "new text")) // 'new text'
 
-// Verify changes were made
-console.log(get("num"))   // 77
-console.log(get("bool"))  // false
-console.log(get("str"))   // 'new text'
+// // Verify changes were made
+// console.log(get("num"))   // 77
+// console.log(get("bool"))  // false
+// console.log(get("str"))   // 'new text'
