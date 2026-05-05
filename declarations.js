7499eda8f6d07122dc46de9048c63b043cf35103
aochuka <ochukaamon@gmail.com>
2026-05-05 12:51:59 +0300

    learning declarations

--- declarations.js
+++ declarations.js
@@ -1,2 +1,30 @@
 const escapeStr = "\`\\/\"\'";
 const arr = [4, '2'];
+
+const obj = Object.freeze({
+    str :"Amon",
+    num : 12,
+    bool : true,
+    undef : undefined
+
+});
+
+const nested = Object.freeze({
+    arr :[4, undefined, 2],
+    obj: {
+        str : "Ochuka",
+        num : 6,
+        bool: true
+    }
+});
+
+Object.freeze(arr);
+
+// Test your frozen objects
+arr[0] = "changed"; // Should not change
+obj.str = "changed"; // Should not change
+nested.obj.bool = "changed"; // Should not change
+
+console.log(arr);          
+console.log(obj.str);       
+console.log(nested.obj.bool); 
