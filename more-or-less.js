680d9e96148037be40c3743b43ba1bac6445cee5
Amonoff <ochukaamon@gmail.com>
2026-05-05 20:34:52 +0300

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,25 @@
+function more(a) {
+    return a + 1
+}
+//const more = a => a + 1
+
+function less(a) {
+    return a - 1
+}
+//const less = a => a - 1
+
+function add(a, b) {
+    return a + b
+}
+//const add = (a,b) => a + b
+
+function sub(a, b) {
+    return a - b
+}
+//const sub = (a,b) => a - b 
+
+// tests
+console.log(more(5))    // 6
+console.log(less(5))    // 4
+console.log(add(5, 3))  // 8
+console.log(sub(5, 3))  // 2
