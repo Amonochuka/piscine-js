f0dbaae2a36228d423c9b07404bdd36fedb0f31c
aochuka <ochukaamon@gmail.com>
2026-05-06 11:38:15 +0300

    using is object  to get better typeOf

--- is.js
+++ is.js
@@ -1,15 +1,6 @@
 const is = {}
 
-
-//short version --> condition ? valueIfTrue : valueIfFalse
-is.num   = (v) => typeof v === "number" && !isNaN(v) ? v : NaN
-// is.num = function(v) {
-//     if (typeof v === "number" && !isNaN(v)) {
-//         return v    // it's a number, return it
-//     } else {
-//         return NaN  // not a number, return NaN
-//     }
-// }
+is.num   = (v) => typeof v === "number" && !isNaN(v)
 is.nan   = (v) => isNaN(v) && typeof v === "number"
 is.str   = (v) => typeof v === "string"
 is.bool  = (v) => typeof v === "boolean"
@@ -21,67 +12,67 @@ is.fun   = (v) => typeof v === "function"
 is.truthy = (v) => !!v
 is.falsy  = (v) => !v
 
-// Test is.num
-console.log(is.num(5)); // true
-console.log(is.num(-5.5)); // true
-console.log(is.num(NaN)); // false
-console.log(is.num("5")); // false
+// // Test is.num
+// console.log(is.num(5)); // true
+// console.log(is.num(-5.5)); // true
+// console.log(is.num(NaN)); // false
+// console.log(is.num("5")); // false
 
-// Test is.nan
-console.log(is.nan(NaN)); // true
-console.log(is.nan(5)); // false
-console.log(is.nan("hello")); // false
+// // Test is.nan
+// console.log(is.nan(NaN)); // true
+// console.log(is.nan(5)); // false
+// console.log(is.nan("hello")); // false
 
-// Test is.str
-console.log(is.str("hello")); // true
-console.log(is.str("")); // true
-console.log(is.str(5)); // false
+// // Test is.str
+// console.log(is.str("hello")); // true
+// console.log(is.str("")); // true
+// console.log(is.str(5)); // false
 
-// Test is.bool
-console.log(is.bool(true)); // true
-console.log(is.bool(false)); // true
-console.log(is.bool(1)); // false
+// // Test is.bool
+// console.log(is.bool(true)); // true
+// console.log(is.bool(false)); // true
+// console.log(is.bool(1)); // false
 
-// Test is.undef
-console.log(is.undef(undefined)); // true
-console.log(is.undef(null)); // false
-console.log(is.undef(0)); // false
+// // Test is.undef
+// console.log(is.undef(undefined)); // true
+// console.log(is.undef(null)); // false
+// console.log(is.undef(0)); // false
 
-// Test is.def
-console.log(is.def(undefined)); // false
-console.log(is.def(null)); // true
-console.log(is.def(0)); // true
+// // Test is.def
+// console.log(is.def(undefined)); // false
+// console.log(is.def(null)); // true
+// console.log(is.def(0)); // true
 
-// Test is.arr
-console.log(is.arr([])); // true
-console.log(is.arr([1, 2, 3])); // true
-console.log(is.arr({})); // false
+// // Test is.arr
+// console.log(is.arr([])); // true
+// console.log(is.arr([1, 2, 3])); // true
+// console.log(is.arr({})); // false
 
-// Test is.obj
-console.log(is.obj({})); // true
-console.log(is.obj(null)); // true
-console.log(is.obj([])); // false
-console.log(is.obj(function () {})); // false
+// // Test is.obj
+// console.log(is.obj({})); // true
+// console.log(is.obj(null)); // true
+// console.log(is.obj([])); // false
+// console.log(is.obj(function () {})); // false
 
-// Test is.fun
-console.log(is.fun(function () {})); // true
-console.log(is.fun(() => {})); // true
-console.log(is.fun({})); // false
+// // Test is.fun
+// console.log(is.fun(function () {})); // true
+// console.log(is.fun(() => {})); // true
+// console.log(is.fun({})); // false
 
-// Test is.truthy
-console.log(is.truthy(true)); // true
-console.log(is.truthy(1)); // true
-console.log(is.truthy("hello")); // true
-console.log(is.truthy(0)); // false
-console.log(is.truthy("")); // false
-console.log(is.truthy(null)); // false
+// // Test is.truthy
+// console.log(is.truthy(true)); // true
+// console.log(is.truthy(1)); // true
+// console.log(is.truthy("hello")); // true
+// console.log(is.truthy(0)); // false
+// console.log(is.truthy("")); // false
+// console.log(is.truthy(null)); // false
 
-// Test is.falsy
-console.log(is.falsy(false)); // true
-console.log(is.falsy(0)); // true
-console.log(is.falsy("")); // true
-console.log(is.falsy(null)); // true
-console.log(is.falsy(undefined)); // true
-console.log(is.falsy(NaN)); // true
-console.log(is.falsy(1)); // false
-console.log(is.falsy("hello")); // false
+// // Test is.falsy
+// console.log(is.falsy(false)); // true
+// console.log(is.falsy(0)); // true
+// console.log(is.falsy("")); // true
+// console.log(is.falsy(null)); // true
+// console.log(is.falsy(undefined)); // true
+// console.log(is.falsy(NaN)); // true
+// console.log(is.falsy(1)); // false
+// console.log(is.falsy("hello")); // false
