c8b0f96105f0134ed213ae88117692e801b13f64
Amonoff <ochukaamon@gmail.com>
2026-05-07 23:01:19 +0300

    block-chain in js

--- block-chain.js
+++ block-chain.js
@@ -1,9 +1,8 @@
-
-
 function blockChain(data, prev = { index: 0, hash: '0' }) {
     const index = prev.index + 1
-// Manually building the start of the string to ensure no hidden formatting issues
-    const hash = hashCode(`${prev.hash}${JSON.stringify(data)}`)
+    // Concatenate index, previous hash, and stringified data directly
+    const hash = hashCode(index + prev.hash + JSON.stringify(data))
+
     const block = {
         index,
         hash,
