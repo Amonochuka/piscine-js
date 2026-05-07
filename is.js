5f31e8c2e2cd9fca935fd46af0d2e1032dbfc2e0
aochuka <ochukaamon@gmail.com>
2026-05-07 09:24:09 +0300

    fix: is.num includes NaN

--- is.js
+++ is.js
@@ -7,7 +7,7 @@ is.bool  = (v) => typeof v === "boolean"
 is.undef = (v) => v === undefined
 is.def   = (v) => v !== undefined
 is.arr   = (v) => Array.isArray(v)
-is.obj   = (v) => v === null || (typeof v === "object" && !Array.isArray(v) && typeof v !== "function")
+is.obj = (v) => typeof v === "object" && !Array.isArray(v) && typeof v !== "function" && v !== null
 is.fun   = (v) => typeof v === "function"
 is.truthy = (v) => !!v
 is.falsy  = (v) => !v
