ce08e8e0f803d9481448373c5ad7c224cda703be
Amonoff <ochukaamon@gmail.com>
2026-05-07 22:47:29 +0300

    block-chain in js

--- block-chain.js
+++ block-chain.js
@@ -1,7 +1,10 @@
+const hashCode = (str) =>
+    ([...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0).toString(36)
 
 function blockChain(data, prev = { index: 0, hash: '0' }) {
     const index = prev.index + 1
-    const hash = hashCode(JSON.stringify({ data, index, hash: prev.hash }))
+// Manually building the start of the string to ensure no hidden formatting issues
+const hash = hashCode(`{"index":${index},"hash":"${prev.hash}","data":${JSON.stringify(data)}}`)
 
     const block = {
         index,
