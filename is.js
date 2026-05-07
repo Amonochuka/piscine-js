4880d7acef00ef335091350859ad68c405c174a6
aochuka <ochukaamon@gmail.com>
2026-05-07 09:09:42 +0300

    fix: is.num includes NaN

--- is.js
+++ is.js
@@ -2,7 +2,6 @@ const is = {}
 
 is.num = (v) => typeof v === "number"  
 is.nan = (v) => isNaN(v) && typeof v === "number"
-is.nan   = (v) => isNaN(v) && typeof v === "number"
 is.str   = (v) => typeof v === "string"
 is.bool  = (v) => typeof v === "boolean"
 is.undef = (v) => v === undefined
