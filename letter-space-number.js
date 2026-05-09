bba43c154353351e43c1555c29c7482ff790aa0b
Amonoff <ochukaamon@gmail.com>
2026-05-09 23:23:56 +0300

    letter-space-number

---
+++ letter-space-number.js
@@ -0,0 +1,21 @@
+function letterSpaceNumber(str) {
+    const result = []
+    
+    for (let i = 0; i < str.length - 2; i++) {
+        const isLetter = (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')
+        const isSpace = str[i + 1] === ' '
+        const isDigit = str[i + 2] >= '0' && str[i + 2] <= '9'
+        const nextIsLetter = (str[i + 3] >= 'a' && str[i + 3] <= 'z') || (str[i + 3] >= 'A' && str[i + 3] <= 'Z')
+        
+        if (isLetter && isSpace && isDigit && !nextIsLetter) {
+            result.push(str[i] + str[i + 1] + str[i + 2])
+        }
+    }
+    
+    return result
+}
+
+// function letterSpaceNumber(str) {
+//     const regex = /[a-zA-Z] \d(?![a-zA-Z])/g
+//     return str.match(regex) || []
+// }
