69b851c3e83398849d1f1a2a81f8706c92b800fc
Amonoff <ochukaamon@gmail.com>
2026-05-09 13:20:43 +0300

    fix: trunc return value

--- cut-corners.js
+++ cut-corners.js
@@ -1,15 +1,12 @@
 function trunc(n) {
     const sign = n < 0 ? -1 : 1
     n = n < 0 ? -n : n  // make positive
-    
+
     let i = 0
-    let step = 1
-    // double step by addition not multiplication
-    while (i + step + step <= n) step += step
-    while (step >= 1) {
-        while (i + step <= n) i += step
-        step -= step / 2  
-    }
+    while (i + 1 <= n) i++  // increment by 1
+
+    // but return with correct sign
+    return sign < 0 ? -i : i
 }
 
 function floor(n) {
