4946c31af974f545ecfed941c4b7a4f42c9c3106
Amonoff <ochukaamon@gmail.com>
2026-05-09 20:36:01 +0300

    manipulation of arrays

--- flat.js
+++ flat.js
@@ -1,10 +1,11 @@
-function flat(arr) {
+function flat(arr, depth = 1) {
     let result = []
     
     for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            for (let j = 0; j < arr[i].length; j++) {
-                result.push(arr[i][j])  // push contents, not the array
+        if (Array.isArray(arr[i]) && depth > 0) {
+            const flattened = flat(arr[i], depth - 1)  // reduce depth each time
+            for (let j = 0; j < flattened.length; j++) {
+                result.push(flattened[j])
             }
         } else {
             result.push(arr[i])
