f8f34f9a80609197f392e5133ced081286d6ecc6
Amonoff <ochukaamon@gmail.com>
2026-05-10 11:10:50 +0300

    finding vowels and manipulating letter after

--- vowel-dots.js
+++ vowel-dots.js
@@ -4,6 +4,8 @@ function vowelDots(str) {
     return str.replace(vowels, '$&.')
 }
 
+// g ;global, find ALL vowels not just first
+// i ;case insensitive, matches A E I O U too
 // tests
 console.log(vowelDots("hello world"))   // 'he.llo wo.rld'
 console.log(vowelDots("Vowels are fun")) // 'Vo.we.ls a.re. fu.n'
