607742e2b879c912707a00c46c4af41f259b8197
Amonoff <ochukaamon@gmail.com>
2026-05-12 10:33:48 +0300

    date-is

---
+++ date-is.js
@@ -0,0 +1,31 @@
+function isValid(date) {
+    return !isNaN(date.getTime())
+}
+
+function isAfter(date1, date2) {
+    return date1.getTime() > date2.getTime()
+}
+
+function isBefore(date1, date2) {
+    return date1.getTime() < date2.getTime()
+}
+
+function isFuture(date) {
+    return isValid(date) && date.getTime() > new Date().getTime()
+}
+
+function isPast(date) {
+    return isValid(date) && date.getTime() < new Date().getTime()
+}
+
+// tests
+const date1 = new Date('2024-01-01')
+const date2 = new Date('2023-01-01')
+const invalid = new Date('invalid')
+
+console.log(isValid(date1))                    // true
+console.log(isValid(invalid))                  // false
+console.log(isAfter(date1, date2))             // true
+console.log(isBefore(date2, date1))            // true
+console.log(isFuture(new Date('2999-01-01'))) // true
+console.log(isPast(new Date('1999-12-31')))   // true
